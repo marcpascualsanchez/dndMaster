76b412233a69363325a15181dd37aaccd9c7012e
marcpascualsanchez <marcpascualsanchez@gmail.com>
2019-11-20 22:43:33 +0100

    Add skills

---
+++ src/components/app-root/skills.ts
@@ -0,0 +1,8 @@
+export const skills = {
+    strength: ['athletics'],
+    dexterity: ['acrobatics', 'sleightOfHand', 'stealth'],
+    constitution: [],
+    intelligence: ['arcana', 'history', 'investigation', 'nature', 'religion'],
+    wisdom: ['animalHandling', 'insight', 'medicine', 'perception', 'survival'],
+    charisma: ['deception', 'intimidation', 'performance', 'persuasion'],
+};
