d300c74d8f332438d676d68d34cd6eb1e2d257e3
marcpascualsanchez <marcpascualsanchez@gmail.com>
2019-11-21 21:47:46 +0100

    Add to root

--- src/components/app-root/app-root.tsx
+++ src/components/app-root/app-root.tsx
@@ -13,6 +13,7 @@ export class AppRoot {
           <ion-route url="/" component="app-home" />
           <ion-route url="/profile/:name" component="app-profile" />
           <ion-route url="/races" component="races-list" />
+          <ion-route url="/new-ability-score" component="new-ability-score" />
         </ion-router>
         <ion-nav />
       </ion-app>
