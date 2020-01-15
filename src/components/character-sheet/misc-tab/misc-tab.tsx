cef20ed0a2d698e098ecf209da074018d48ec6b1
marcpascualsanchez <marcpascualsanchez@gmail.com>
2020-01-15 21:32:30 +0100

    Add item list and money table WIP

--- src/components/character-sheet/misc-tab/misc-tab.tsx
+++ src/components/character-sheet/misc-tab/misc-tab.tsx
@@ -1,5 +1,6 @@
-import { Component, h, Prop } from '@stencil/core';
+import { Component, h, Prop, State } from '@stencil/core';
 import { ICharacter } from '../../../models/Character';
+import { IItem } from '../../../models/classes/Class';
 
 @Component({
   tag: 'misc-tab',
@@ -7,22 +8,50 @@ import { ICharacter } from '../../../models/Character';
   shadow: true
 })
 export class MiscTab {
-
   @Prop() character: ICharacter;
+  @State() items: IItem[];
+
+  constructor() {
+    this.items = this.character.equipment.items;
+  }
+
+  getItemsList(items: IItem[]) {
+    return items.map((a) =>
+      <ion-row custom-value={a}>
+        <ion-col size="1">{a.amount}</ion-col>
+        <ion-col size="11">{a.name}</ion-col>
+      </ion-row>
+    );
+  }
 
   render() {
+    this.character.saveLocalCharacter();
     return (
       <ion-grid>
         <ion-row>
-          <ion-col size="6">this is</ion-col>
-          <ion-col size="6">misc</ion-col>
+          <ion-col size="2">Copper</ion-col>
+          <ion-col size="2">Silver</ion-col>
+          <ion-col size="2">Gold</ion-col>
+          <ion-col size="2">Electrum</ion-col>
+          <ion-col size="2">Platinum</ion-col>
         </ion-row>
         <ion-row>
-          <ion-col size="6">bye</ion-col>
-          <ion-col size="6">bye</ion-col>
+          <ion-col size="2">{this.character.money}<ion-icon name="cash"></ion-icon></ion-col>
+          <ion-col size="2">{this.character.money}<ion-icon name="cash"></ion-icon></ion-col>
+          <ion-col size="2">{this.character.money}<ion-icon name="cash"></ion-icon></ion-col>
+          <ion-col size="2">{this.character.money}<ion-icon name="cash"></ion-icon></ion-col>
+          <ion-col size="2">{this.character.money}<ion-icon name="cash"></ion-icon></ion-col>
+        </ion-row>
+        <ion-row>
+          <ion-grid>
+            <ion-row>
+              {/* <h3>Items<ion-icon slot="end" name="add-circle" color="primary" onClick={() => this.showItemsModal()}></ion-icon></h3> */}
+              <h3>Items</h3>
+            </ion-row>
+            {this.getItemsList(this.character.equipment.items)}
+          </ion-grid>
         </ion-row>
       </ion-grid>
     );
   }
-
 }
